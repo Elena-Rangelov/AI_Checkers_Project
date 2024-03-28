c6d99f15f2932ae55fae30153257e4127eed1d04
Elena Rangelov <elenarangelov04@gmail.com>
2024-03-28 19:08:59 -0400

    player and state setup

--- player.py
+++ player.py
@@ -1,1 +1,41 @@
 
+import numpy as np
+
+BOARD_COLS = 8
+BOARD_ROWS = 8
+
+class BotPlayer():
+    def __init__(self):
+        # christina
+        # initiate function
+    
+    def getHash(self, board):
+        return str(board.reshape(BOARD_COLS * BOARD_ROWS))
+
+    def chooseAction(self, pos, board):
+        # el
+        # choose action
+
+    def addStates(self, board_hash):
+        self.states.append(board_hash)
+
+    def feedReward(self, reward):
+        # wren
+        # assign rewards
+
+    def play(self, rounds=100):
+        # christina
+        # play
+
+    def reset(self):
+        self.states = []
+
+    def savePolicy(self):
+        # wren
+        # save policy
+
+    def loadPolicy(self):
+        # wren
+        # load policy
+
+    ########### finish later
--- state.py
+++ state.py
@@ -1,1 +1,64 @@
+import numpy as np
+
+BOARD_COLS = 8
+BOARD_ROWS = 8
+
+class State():
+    def __init__(self, p1, p2):
+        self.board = [[0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0],
+                    [0, 1, 0, 1, 0, 1, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [2, 0, 2, 0, 2, 0, 2, 0],
+                    [0, 2, 0, 2, 0, 2, 0, 2],
+                    [2, 0, 2, 0, 2, 0, 2, 0]]
+
+        self.p1 = p1
+        self.p2 = p2
+
+        self.isEnd = False
+        self.boardHash = None
+
+    def getHash(self):
+        self.boardHash = str(self.board.reshape(BOARD_COLS * BOARD_ROWS))
+        return self.boardHash
+
+    def getAvailablePositions(self, p):
+        positions = {}
+        for i in range(self.BOARD_COLS):
+            for j in range(self.BOARD_ROWS):
+                if p.first:
+                    self.board[i][j] == 1:
+                        if self.board[i+1][j+1] == 0:
+                            positions[(i, j)] += 1
+                    elif self.board[i][j] == 3:
+                # finish
+                # el
+
+    def winner(self):
+        # checking end state
+        # christina
+
+    def updateState(self, pos):
+        # kaan
+        # update the board
+
+    def reset(self):
+        # kaan
+        # reset board
+
+    def giveReward(self):
+        # kaan
+        # assign rewards
+
+    ############################# FOR LATER
+    def playHuman(self):
+        # play human
+
+    def showBoard(self):
+        # gui display
+    
+
+
 
