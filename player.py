db0c4d11491c0e501e37a272bd79a99fb5badc79
Wren Poremba <wrenporemba@gmail.com>
2024-04-04 17:05:14 +0000

    First draft of feedReward, savePolicy, and loadPolicy

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import random
+import pickle
 
 BOARD_COLS = 8
 BOARD_ROWS = 8
@@ -38,8 +39,15 @@ class BotPlayer():
         self.states.append(board_hash)
 
     def feedReward(self, reward):
-        # wren
-        # assign rewards
+        # Loop through the states the player went through
+        for curr in reversed(self.states):
+            # Initializes state value if not encountered before
+            if self.states_value.get(curr) is None:
+                self.states_value[curr] = 0
+            # Use value iteration formula
+            self.states_value[curr] += self.lr * (self.decay_gamma * reward - self.states_value[curr])
+            # Updates reward for next iteration
+            reward = self.states_value[curr]
 
     def play(self, rounds=100):
         # christina
@@ -48,12 +56,15 @@ class BotPlayer():
     def reset(self):
         self.states = []
 
+    # Using pickle module to save and load policies
     def savePolicy(self):
-        # wren
-        # save policy
+        fw = open('policy_' + str(self.name), 'wb')
+        pickle.dump(self.states_value, fw)
+        fw.close()
 
-    def loadPolicy(self):
-        # wren
-        # load policy
+    def loadPolicy(self, file):
+        fr = open(file, 'rb')
+        self.states_value = pickle.load(fr)
+        fr.close()
 
     ########### finish later
